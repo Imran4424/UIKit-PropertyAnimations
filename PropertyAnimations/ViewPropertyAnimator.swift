0ff4cecdb4732354cc2bdd6f239ba6437b08fe5a
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-03-13 00:58:14 +0600

    background colour

--- PropertyAnimations/ViewPropertyAnimator.swift
+++ PropertyAnimations/ViewPropertyAnimator.swift
@@ -9,9 +9,7 @@ import UIKit
 
 class ViewPropertyAnimator: UIViewController {
     let slider = UISlider()
-    var animator: UIViewPropertyAnimator!
-    let box = UIView()
-    var boxWidthAnchor: NSLayoutConstraint!
+    let animator = UIViewPropertyAnimator(duration: 1, curve: .linear)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,44 +20,25 @@ class ViewPropertyAnimator: UIViewController {
     private func setupViews() {
         view.backgroundColor = .red
         
-//        animator.addAnimations {
-//            self.view.backgroundColor = .white
-//            self.box.backgroundColor = .blue
-//            self.view.layoutIfNeeded()
-//        }
+        animator.addAnimations {
+            self.view.backgroundColor = .white
+        }
         
         slider.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(slider)
-        
-        box.translatesAutoresizingMaskIntoConstraints = false
-        box.backgroundColor = .green
-        view.addSubview(box)
     }
     
     private func layoutViews() {
-        slider.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 150).isActive = true
+        slider.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 200).isActive = true
         slider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         slider.widthAnchor.constraint(equalToConstant: view.frame.width - 100).isActive = true
         slider.addTarget(self, action: #selector(handleSliderChanged), for: .allEvents)
-        
-        boxWidthAnchor = box.widthAnchor.constraint(equalToConstant: 100)
-        boxWidthAnchor.isActive = true
-        box.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        box.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        box.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
 // MARK: - Actions
 extension ViewPropertyAnimator {
     @objc private func handleSliderChanged() {
-        boxWidthAnchor.constant = 100 + CGFloat((slider.value * 100))
-//        animator.fractionComplete = CGFloat(slider.value)
-        animator = UIViewPropertyAnimator(duration: 2, dampingRatio: 0.5, animations: {
-            self.box.backgroundColor = .blue
-            self.view.backgroundColor = .white
-            self.view.layoutIfNeeded()
-        })
-        animator.startAnimation()
+        animator.fractionComplete = CGFloat(slider.value)
     }
 }
