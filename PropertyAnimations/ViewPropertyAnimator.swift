695a07dc9fd3550f2237249a41426deea8ef089d
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-03-13 01:07:27 +0600

    add image blur

--- PropertyAnimations/ViewPropertyAnimator.swift
+++ PropertyAnimations/ViewPropertyAnimator.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewPropertyAnimator: UIViewController {
     let slider = UISlider()
     let animator = UIViewPropertyAnimator(duration: 1, curve: .linear)
+    var imageView = UIImageView(image: UIImage(named: "himalaya"))
+    var blurView: UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .prominent))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +23,24 @@ class ViewPropertyAnimator: UIViewController {
         view.backgroundColor = .red
         
         animator.addAnimations {
-            self.view.backgroundColor = .white
+            self.blurView.alpha = 1
+            self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
         }
+        
+        imageView.frame = view.frame
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        
+        blurView.frame = view.frame
+        blurView.alpha = 0.1
+        view.addSubview(blurView)
         
         slider.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(slider)
     }
     
     private func layoutViews() {
-        slider.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 200).isActive = true
+        slider.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150).isActive = true
         slider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         slider.widthAnchor.constraint(equalToConstant: view.frame.width - 100).isActive = true
         slider.addTarget(self, action: #selector(handleSliderChanged), for: .allEvents)
